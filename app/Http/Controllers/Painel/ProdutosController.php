5288f4f618f69f2448061f903414a26d0932a30c
RuntimeMendes <runtime_mendes@live.com>
2018-07-28 11:19:20 -0300

    trabalhando em cima dos inserts

--- app/Http/Controllers/Painel/ProdutosController.php
+++ app/Http/Controllers/Painel/ProdutosController.php
@@ -9,6 +9,13 @@ use App\Models\Painel\Produtos;
 class ProdutosController extends Controller
 {
     //
+   private $produtos;
+
+   function __construct($produto) {
+    $this->produto = $produto;
+}
+
+    
 
    public function index(Produtos $produtos){
 
@@ -19,7 +26,25 @@ class ProdutosController extends Controller
     return view('Painel.Produtos.index',compact('produtosConsulta'));
    }
 
+   public function testes (){
+
+
+    $prod = $this->produto;
+    
+    $prod->nome = 'caderno';
+    $prod->numero = 12321;
+    $prod->ativo = 1;
+    $prod->categoria = 'eletronico';
+    $prod->descricao = 'Material para  escola';
+
+
+    return 'testando ricardo =]  salvo';
+
+
+   }
 
 
 
 }
+
+
